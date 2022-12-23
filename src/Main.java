27bb309bc93ff3d8c50600870fef97a6117e77a7
Lexa <karpuk.lesha@mail.ru>
2022-12-23 03:31:10 +0300

    new info

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.print(args [0]);
         System.out.println("Alexey Karpuk! " + args [1]);
+        System.out.println(args [2]);
+        System.out.println(args [3]);
         Scanner sc = new Scanner(System.in);
         int y = 2022;
         int a = sc.nextInt();
         int x = y - a;
-        System.out.print(args [2]);
-        System.out.println(x + args [3]);
-        System.out.println(args [4]);
+        System.out.print(args [4]);
+        System.out.println(x + args [5]);
+        System.out.println(args [6]);
         int b = 31;
         int c = sc.nextInt();
         int v = b - c;
-        System.out.print(args [5] + v);
-        System.out.println(args [6] + args [7]);
+        System.out.print(args [7] + v);
+        System.out.println(args [8] + args [9]);
     }
 }
